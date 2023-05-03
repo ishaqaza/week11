77996337d2cb7b33258624ead473fc14b7e9f368
Ishaq Azami <ishaqaza@buffalo.edu>
2023-05-03 14:07:42 -0400

    update table

--- flask_app/mysql_create_table.py
+++ flask_app/mysql_create_table.py
@@ -4,11 +4,11 @@ conn = sql.connect(host="localhost", user="flask", password="ubuntu", database="
 cur = conn.cursor()
 
 cmd = "CREATE TABLE students (\
-    sid INT NOT NULL AUTO_INCREMENT PRIMARY KEY, \
-    name VARCHAR(30) NOT NULL,\
-    addr VARCHAR(30), \
-    city VARCHAR(30), \
-    zip VARCHAR(30))"
+    EmpID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, \
+    EmpName VARCHAR(30) NOT NULL,\
+    EmpGedner VARCHAR(30), \
+    EmpPhone VARCHAR(30), \
+    EmpBdate VARCHAR(30))"
 
 cur.execute(cmd)
 conn.close()
