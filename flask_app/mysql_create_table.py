2e4e1ce898d70b85579ceb7e2a79faa2bd83b997
Ishaq Azami <ishaqaza@buffalo.edu>
2023-05-03 14:11:49 -0400

    update table

--- flask_app/mysql_create_table.py
+++ flask_app/mysql_create_table.py
@@ -3,7 +3,7 @@ import mysql.connector as sql
 conn = sql.connect(host="localhost", user="flask", password="ubuntu", database="flask_db")
 cur = conn.cursor()
 
-cmd = "CREATE TABLE students (\
+cmd = "CREATE TABLE Employee (\
     EmpID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, \
     EmpName VARCHAR(30) NOT NULL,\
     EmpGedner VARCHAR(30), \
